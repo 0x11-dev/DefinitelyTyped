d8a915a1093e97785353b6e0007cab18e2498bf3
0x11 <levin-du@users.noreply.github.com>
2019-07-02 00:32:18 +0800

    [CodeMirror] change host argument to optional

--- types/codemirror/index.d.ts
+++ types/codemirror/index.d.ts
@@ -11,7 +11,7 @@
 export = CodeMirror;
 export as namespace CodeMirror;
 
-declare function CodeMirror(host: HTMLElement, options?: CodeMirror.EditorConfiguration): CodeMirror.Editor;
+declare function CodeMirror(host?: HTMLElement, options?: CodeMirror.EditorConfiguration): CodeMirror.Editor;
 declare function CodeMirror(callback: (host: HTMLElement) => void , options?: CodeMirror.EditorConfiguration): CodeMirror.Editor;
 
 declare namespace CodeMirror {
